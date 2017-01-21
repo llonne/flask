514a099ab24e2bf1539140765592e5b8c170f0df
llonne <lindsey.lonne@gmail.com>
2017-01-21 12:04:48 +0000

    Working version of flask skills assessment part one

--- server.py
+++ server.py
@@ -9,8 +9,41 @@ app = Flask(__name__)
 app.secret_key = "ABC"
 
 
-# YOUR ROUTES GO HERE
+@app.route("/")
+def index():
+    """Show homepage."""
 
+    return render_template("index.html")
+
+
+@app.route("/application-form")
+def application_form():
+    """Show application form."""
+
+    job_list = ["Software Engineer", "QA Engineer", "Product Manager"]
+
+    return render_template("application-form.html", jobs=job_list)
+
+
+# @app.route("/application-success", methods=['GET', 'POST'])
+@app.route("/application-success")
+def application_success():
+    """Show application form."""
+
+    fname = request.args.get("firstname")
+    lname = request.args.get("lastname")
+    salary = format_currency(float(request.args.get("salaryreq")))
+    title = request.args.get("jobtitle")
+
+    return render_template("application-response.html",
+                           first_name=fname,
+                           last_name=lname,
+                           salary_req=salary,
+                           job_title=title)
+
+
+def format_currency(value):
+    return "${:,.2f}".format(value)
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
